6cc4742103f580379dadf4b852c6f91c6355d5a2
sol <taylorkwon92@gmail.com>
2022-01-17 09:05:59 +0900

    chore : scss 와 module css 적용

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div>Hi</div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/Main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './Main.module.scss';
+
+const Main = () => {
+  return <div className="bg"></div>;
+};
+
+export default Main;
