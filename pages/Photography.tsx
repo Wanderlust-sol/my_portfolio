46d64babfd4488fc5ce40bb84e4acec67c38cf49
sol <taylorkwon92@gmail.com>
2022-01-19 16:19:05 +0900

    feat : photography 페이지 작성

--- pages/Photography.tsx
+++ pages/Photography.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import Header from '../components/Header';
+import style from './Photography.module.scss';
+import mockData from '../dummy/mock_photo.json';
+import Image from 'next/image';
 
 const Photography = () => {
   return (
-    <div>
-      <Header />
-      <div>hi</div>
-    </div>
+    <>
+      <div className={style.bg}>
+        {mockData.map(({ url, location }, index) => {
+          return (
+            <div className={style.wrapper} key={index}>
+              <div className={style.photoBox}>
+                <Image
+                  src={url}
+                  width="100%"
+                  height="100%"
+                  layout="responsive"
+                  alt="photography"
+                  priority
+                />
+              </div>
+              <div>{location}</div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
